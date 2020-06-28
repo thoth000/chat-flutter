e6785c85281d5d9859e754b0d0b9b2f771bd5c33
thoth000 <snym719a@gmail.com>
2020-06-28 16:15:43 +0900

    AppColorクラスの削除

--- lib/config/app_color.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColor{
-  static final Color baseColor = Color(0xff707070);
-  static final Color backColor = Color(0xffEEEEEE);
-  static final Color accentColor = Colors.blue;
-}
