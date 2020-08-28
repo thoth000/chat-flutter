e517db3151b7b9fa9715e4b1c7ed567c72236b4f
tomoki inoue <>
2020-08-28 14:55:23 +0900

    作成したroomのidでusersのコレクション直下にroom_settingを作成

--- lib/services/firebase_room_service.dart
+++ lib/services/firebase_room_service.dart
@@ -4,13 +4,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirebaseRoomService {
   final Firestore _db = Firestore.instance;
 
-  Future<void> setRoomData(Room room) async {
+  Future<String> setRoomData(Room room) async {
     final roomData = {
       'name': room.name,
       'members': room.members,
       'imgUrl': room.imgUrl,
       'lastMessage': room.lastMessage,
     };
-    await _db.collection('message/v1/rooms').add(roomData);
+    final DocumentReference docRef =
+        await _db.collection('message/v1/rooms').add(roomData);
+    return docRef.documentID;
+  }
+
+  Future<void> setRoomSetting(String userId, String roomId) async {
+    final settingData = {
+      'allowNotification': false,
+    };
+
+    await _db
+        .collection('message/v1/users/$userId/room_setting')
+        .add(settingData);
   }
 }
--- lib/ui/pages/create_room/create_room_controller.dart
+++ lib/ui/pages/create_room/create_room_controller.dart
@@ -15,6 +15,7 @@ class CreateRoomController with ChangeNotifier {
   File selectedImageFile;
   Room room = Room();
   final Authenticator authenticator;
+  final FirebaseRoomService _firebaseRoomService = FirebaseRoomService();
 
   Future<void> createRoom(List<User> members, String roomName) async {
     final imagePath = selectedImageFile != null
@@ -38,7 +39,9 @@ class CreateRoomController with ChangeNotifier {
       lastMessage: '',
     );
 
-    await FirebaseRoomService().setRoomData(room);
+    final String roomId = await _firebaseRoomService.setRoomData(room);
+
+    await _firebaseRoomService.setRoomSetting(myId, roomId);
   }
 
   Future<void> selectProfileImage() async {
