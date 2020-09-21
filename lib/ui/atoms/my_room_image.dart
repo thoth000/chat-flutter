5aa876c591dda8db918c24fc2194da5daf682632
thoth000 <snym719a@gmail.com>
2020-09-21 22:31:35 +0900

    正しい変数名付けとRoomEditController
    つまり、細かなfix

--- lib/ui/atoms/my_room_image.dart
+++ lib/ui/atoms/my_room_image.dart
@@ -1,5 +1,7 @@
 import 'package:chat_flutter/ui/pages/room/room_controller.dart';
+import 'package:chat_flutter/ui/pages/room/room_edit_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyRoomImage extends StatelessWidget {
   const MyRoomImage({Key key, this.size, this.roomController})
@@ -9,12 +11,14 @@ class MyRoomImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (roomController.selectedImageFile != null) {
+    final RoomEditController roomEditController =
+        Provider.of<RoomEditController>(context);
+    if (roomEditController.selectedImageFile != null) {
       return SizedBox(
         width: size,
         height: size,
         child: CircleAvatar(
-          backgroundImage: FileImage(roomController.selectedImageFile),
+          backgroundImage: FileImage(roomEditController.selectedImageFile),
         ),
       );
     } else if (roomController.room.imgUrl != '' &&
